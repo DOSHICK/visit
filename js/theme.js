75b0b6b5cb34720d49d3d337fb6f686e9ade3280
zubritckiy Ivan <doshick203@yandex.ru>
2022-01-05 15:23:42 +0300

    add theme change feature, but it's doesn't work

--- js/theme.js
+++ js/theme.js
@@ -1,2 +1,31 @@
 // theme.js
-console.log("theme.js is connected")
+console.log("theme.js is connected");
+
+let themeBtn = document.getElementById("theme-btn");
+let themeMoon = document.getElementById("theme-moon");
+let themeSun = document.getElementById("theme-sun");
+let themeCheck = 0;
+
+themeBtn.onclick = function(){
+
+  if(themeCheck = 0){
+    document.body.classList.add("light-theme");
+    console.log(themeCheck);
+  }else{
+    document.body.classList.add("dark-theme");
+  }
+
+  if(themeCheck = 0){
+    themeCheck = 1
+  }else{
+    themeCheck = 0
+  }
+  console.log(themeCheck)
+
+    themeMoon.classList.toggle("display-none");
+    themeSun.classList.toggle("display-none");
+    console.log("theme is changed")
+
+  
+  console.log(themeCheck)
+}
