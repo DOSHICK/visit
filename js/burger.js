59f63c836c89d868f8bf9dd7d10b905c5b1d1670
zubritckiy Ivan <doshick203@yandex.ru>
2022-01-11 21:22:20 +0300

    finished burger.js 1.2

--- js/burger.js
+++ js/burger.js
@@ -3,36 +3,36 @@ console.log("burger.js is connected");
 
 let header = document.querySelector("header");
 let burgerBtn = document.getElementById("hamburger-menu");
-let navList = document.getElementById("nav__list");
-let link = document.querySelectorAll("li.nav__item > a")
+let navList = document.getElementById("nav__list")
 let burgerCondition = "closed";
 
-function burgerVisibility(){
+function burgerVisibility() {
   header.classList.toggle("burger-visible");
-    console.log("burger condition changed");
-    if (header.className === ""){
-      navList.classList.add("display-none")
-      navList.classList.remove("nav__list")
-      navList.classList.add("nav__list")
-    }else{
-      navList.classList.remove("display-none")
-    }
-}
-
-link.onclick = function(){
-  // burgerVisibility();
-  alert("hola")
-}
-
-burgerBtn.onclick = function(){
-  burgerVisibility();
-}
-
-  if (window.matchMedia("(min-width: 500px)").matches) {
-    console.log("all ok")
-    burgerBtn.classList.add("display-none")
+  console.log("burger condition changed");
+  if (header.className === "") {
+    navList.classList.add("display-none")
+    navList.classList.remove("nav__list")
+    navList.classList.add("nav__list")
   } else {
+    navList.classList.remove("display-none")
+  }
+}
+
+function checkResolution() {
+  if (window.matchMedia("(min-width: 500px)").matches) {
+    burgerBtn.classList.add("display-none");
+    navList.classList.remove("display-none")
+  } else {
+    burgerBtn.classList.remove("display-none")
     navList.classList.add("display-none")
   }
+}
+checkResolution()
 
+burgerBtn.onclick = function () {
+  burgerVisibility()
+}
+window.addEventListener("resize", function () {
+  checkResolution()
+});
 
