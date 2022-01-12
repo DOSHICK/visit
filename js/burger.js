6152a97f69a3774d0ce1ed4b86bf4f9febd7a93c
zubritckiy Ivan <doshick203@yandex.ru>
2022-01-12 15:59:18 +0300

    сделал закрывание burger при нажатии на .nav__link

--- js/burger.js
+++ js/burger.js
@@ -4,7 +4,10 @@ console.log("burger.js is connected");
 let header = document.querySelector("header");
 let burgerBtn = document.getElementById("hamburger-menu");
 let navList = document.getElementById("nav__list")
+let navLink = navList.querySelectorAll("a.nav__link");
 let burgerCondition = "closed";
+
+console.log(navLink)
 
 function burgerVisibility() {
   header.classList.toggle("burger-visible");
@@ -32,6 +35,18 @@ checkResolution()
 burgerBtn.onclick = function () {
   burgerVisibility()
 }
+
+
+
+
+  for (let navLinkCounter = 0; navLinkCounter <= navLink.length; navLinkCounter++){
+    navLink[navLinkCounter].onclick = function() {
+      console.log("hola");
+      burgerVisibility()
+    }
+  }
+
+
 window.addEventListener("resize", function () {
   checkResolution()
 });
